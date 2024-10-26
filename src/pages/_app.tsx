02e0ed3efb500cd4b0668792fcfd56cee7ad83b7
wylin <macbook@Macbooks-MacBook-Pro.local>
2024-10-25 23:24:11 -0500

    updated favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         {/* Favicons */}
-        <link rel="icon" href="/favicon-32x32.png" />
+        <link rel="icon" href="/favicon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
